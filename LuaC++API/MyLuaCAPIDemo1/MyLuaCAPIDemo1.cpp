701fe6b6c6ae195eca514ccdb85206f5fa03676d
Jeff <1571098645@qq.com>
2020-12-25 18:36:20 +0800

    【第一个lua解释器】

--- LuaC++API/MyLuaCAPIDemo1/MyLuaCAPIDemo1.cpp
+++ LuaC++API/MyLuaCAPIDemo1/MyLuaCAPIDemo1.cpp
@@ -1,4 +1,5 @@
 ﻿#include <stdio.h>
+#include <string.h>
 
 extern "C" {
 #include "lua.h"
@@ -7,8 +8,25 @@ extern "C" {
 }
 int main()
 {
-    //std::cout << "Hello World!\n";
 
+	char buff[256];
+	int error;
+	lua_State *L = luaL_newstate();   //新建一个LuaState
+	luaL_openlibs(L);							 //打开标准库
 
+	while (fgets(buff, sizeof(buff), stdin) != NULL)
+	{
+		error = luaL_loadbuffer(L, buff, strlen(buff), "line") || lua_pcall(L, 0, 0, 0);
+
+		if (error)
+		{
+			fprintf(stderr, "%s", lua_tostring(L, -1));
+			lua_pop(L, 1);			//从栈中弹出错误消息
+		}
+	}
+
+	lua_close(L);
+
+	return 0;
 }
 
